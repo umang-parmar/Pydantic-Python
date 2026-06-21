becfd6ab2f35218d2b8abb570d67c821ddd2c55e
umang-parmar <umang2502parmar@gmail.com>
2026-06-21 22:39:50 +0530

    first_model make

--- 01_basics/first_model.py
+++ 01_basics/first_model.py
@@ -1,1 +1,12 @@
-print("start of pydantic journey")
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id:int
+    name:str
+    is_active:bool
+
+input_data = {"id":101,"name":"Umang","is_active":True}
+
+user = User(**input_data)
+# ** is expand-unpack the dictionary(inside value scatered around treat Not only one object)
+print(user)
