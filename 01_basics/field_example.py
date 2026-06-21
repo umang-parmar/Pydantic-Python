5a6448c44e4bf5e6c69af2cfce643f55ea902b57
umang-parmar <umang2502parmar@gmail.com>
2026-06-22 00:02:16 +0530

    last one complete ex

--- 01_basics/field_example.py
+++ 01_basics/field_example.py
@@ -11,3 +11,11 @@ class BlogPost(BaseModel):
     title: str
     content : str
     image_url : Optional[str] = None #string or None 
+
+cart_data = {
+    "user_id":123,
+    "items":["Laptop","Mouse","Keyboard"],
+    "quantities":{"laptop":1,"mouse":2,"keyboard":3}
+}
+
+cart = Cart(**cart_data)
